83c87b410bc7f609699930c1b3352de8ac5a32c6
emilibasilio <emilinicolebasilio.garcia@gmail.com>
2021-06-15 17:26:35 -0600

    Arreglo de una fila

--- APP1/APP1/Program.cs
+++ APP1/APP1/Program.cs
@@ -47,7 +47,7 @@ namespace APP1
                     Console.WriteLine("\n Datos de los Contactos: ");
                     for(int f=0; f<15; f++)
                     {
-                        Console.WriteLine(mat[0,f]+""+mat[1,f]+""mat[3,f]+""+mat[4,f]);
+                        Console.WriteLine(mat[0,f]+""+mat[1,f]+""+mat[2,f]+""+mat[3,f]+""+mat[4, f]);
                     }
                 }
                 if(op==3)
